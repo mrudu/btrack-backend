b6f04f1cd7d10b92c4ec9dc561dec515b636fe77
Mru <=>
2014-08-05 20:01:58 +0530

    Initial Commit

---
+++ quickstart/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User, Group
+from quickstart.models import LANGUAGE_CHOICES, STYLE_CHOICES, Customer, Project, Component
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = User
+		fields = ('url', 'username', 'email', 'groups')
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Group
+		fields = ('url', 'name')
+
+class CustomerSerializer(serializers.ModelSerializer):
+	components = serializers.RelatedField(True)
+	class Meta:
+		model= Customer
+
+class ComponentSerializer(serializers.ModelSerializer):
+	projects = serializer.RelatedField(True)
+	class Meta:
+		model= Component
+
+class ProjectSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Project
+
